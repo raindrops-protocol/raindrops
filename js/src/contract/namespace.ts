34fd203e8c7be5bb5f8cc9eaa79e84c08cef1ba6
Long Banana <skews_digraph_0r@icloud.com>
2022-06-07 16:03:26 -0700

    integrate sol-kit and sol-command to namespace

--- js/src/contract/namespace.ts
+++ js/src/contract/namespace.ts
@@ -1,181 +1,42 @@
-import { web3, Program, BN, AnchorProvider } from "@project-serum/anchor";
-import { SystemProgram } from "@solana/web3.js";
-import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
-import log from "loglevel";
+import { Program } from "@raindrops-protocol/sol-kit";
+import { web3 } from "@project-serum/anchor";
 
-import { NAMESPACE_ID, TOKEN_PROGRAM_ID } from "../constants/programIds";
-import {
-  decodeNamespace,
-  Namespace,
-  PermissivenessSettings,
-} from "../state/namespace";
+import * as NamespaceInstruction from "../instructions/namespace";
+import { NAMESPACE_ID } from "../constants/programIds";
+import { PREFIX } from "../constants/namespace";
+import { Namespace } from "../state/namespace";
 import { getNamespacePDA } from "../utils/pda";
 
-import { ObjectWrapper } from "./common";
-import { getCluster } from "../utils/connection";
-import { sendTransactionWithRetry } from "../utils/transactions";
+export class NamespaceProgram extends Program.Program {
+  declare instruction: NamespaceInstruction.Instruction;
+  static PREFIX = PREFIX;
+  PROGRAM_ID = NAMESPACE_ID;
 
-function convertNumbersToBNs(data: any) {
-  if (data.desiredNamespaceArraySize) {
-    data.desiredNamespaceArraySize = new BN(data.desiredNamespaceArraySize);
-  }
-  if (data.bump) {
-    data.bump = new BN(data.bump);
-  }
-}
+  constructor() {
+    super();
 
-export class NamespaceWrapper
-  implements ObjectWrapper<Namespace, NamespaceProgram>
-{
-  program: NamespaceProgram;
-  key: web3.PublicKey;
-  object: Namespace;
-  data: Buffer;
-
-  constructor(args: {
-    program: NamespaceProgram;
-    key: web3.PublicKey;
-    object: Namespace;
-    data: Buffer;
-  }) {
-    this.program = args.program;
-    this.key = args.key;
-    this.object = args.object;
-    this.data = args.data;
-  }
-}
-
-export interface InitializeNamespaceAccounts {
-  namespace?: web3.PublicKey;
-  mint: web3.PublicKey;
-  metadata: web3.PublicKey;
-  masterEdition: web3.PublicKey;
-}
-
-export interface InitializeNamespaceArgs {
-  desiredNamespaceArraySize: BN;
-  uuid: string;
-  prettyName: string;
-  permissivenessSettings: PermissivenessSettings;
-  whitelistedStakingMints: web3.PublicKey[];
-}
-
-export interface InitializeNamespaceAdditionalArgs {}
-
-export class NamespaceInstruction {
-  id: web3.PublicKey;
-  program: Program;
-
-  constructor(args: { id: web3.PublicKey; program: Program }) {
-    this.id = args.id;
-    this.program = args.program;
+    this.instruction = new NamespaceInstruction.Instruction({ program: this });
   }
 
   async initializeNamespace(
-    args: InitializeNamespaceArgs,
-    accounts: InitializeNamespaceAccounts,
-    _additionalArgs: InitializeNamespaceAdditionalArgs = {}
-  ) {
-    const [namespacePDA, _namespaceBump] = await getNamespacePDA(accounts.mint);
-
-    convertNumbersToBNs(args);
-
-    const remainingAccounts = args.whitelistedStakingMints.map((mint) => {
-      return { pubkey: mint, isWritable: false, isSigner: false };
-    });
-
-    return [
-      await this.program.methods
-        .initializeNamespace(args)
-        .accounts({
-          namespace: namespacePDA,
-          mint: accounts.mint,
-          metadata: accounts.metadata,
-          masterEdition: accounts.masterEdition,
-          payer: (this.program.provider as AnchorProvider).wallet.publicKey,
-          tokenProgram: TOKEN_PROGRAM_ID,
-          systemProgram: SystemProgram.programId,
-          rent: web3.SYSVAR_RENT_PUBKEY,
-        })
-        .remainingAccounts(remainingAccounts)
-        .instruction(),
-    ];
-  }
-}
-
-export class NamespaceProgram {
-  id: web3.PublicKey;
-  program: Program;
-  instruction: NamespaceInstruction;
-
-  constructor(args: { id: web3.PublicKey; program: Program }) {
-    this.id = args.id;
-    this.program = args.program;
-    this.instruction = new NamespaceInstruction({
-      id: this.id,
-      program: this.program,
-    });
-  }
-
-  async initializeNamespace(
-    args: InitializeNamespaceArgs,
-    accounts: InitializeNamespaceAccounts
+    args: NamespaceInstruction.InitializeNamespaceArgs,
+    accounts: NamespaceInstruction.InitializeNamespaceAccounts,
   ): Promise<void> {
     const instruction = await this.instruction.initializeNamespace(
       args,
       accounts
     );
 
-    await sendTransactionWithRetry(
-      (this.program.provider as AnchorProvider).connection,
-      (this.program.provider as AnchorProvider).wallet,
+    await this.sendWithRetry(
       instruction,
       []
     );
   }
 
-  async fetchNamespace(mint: web3.PublicKey): Promise<NamespaceWrapper> {
+  async fetchNamespace(mint: web3.PublicKey): Promise<Namespace> {
     let namespacePDA = (await getNamespacePDA(mint))[0];
 
-    let namespaceObj = await (
-      this.program.provider as AnchorProvider
-    ).connection.getAccountInfo(namespacePDA);
-
-    const namespaceDecoded = decodeNamespace(namespaceObj.data);
-    namespaceDecoded.program = this.program;
-
-    return new NamespaceWrapper({
-      program: this,
-      key: namespacePDA,
-      data: namespaceObj.data,
-      object: namespaceDecoded,
-    });
-  }
-}
-
-export async function getNamespaceProgram(
-  anchorWallet: NodeWallet | web3.Keypair,
-  env: string,
-  customRpcUrl: string
-): Promise<NamespaceProgram> {
-  if (customRpcUrl) log.debug("USING CUSTOM RPC URL:", customRpcUrl);
-
-  const solConnection = new web3.Connection(customRpcUrl || getCluster(env));
-
-  if (anchorWallet instanceof web3.Keypair) {
-    anchorWallet = new NodeWallet(anchorWallet);
-  }
-
-  const provider = new AnchorProvider(solConnection, anchorWallet, {
-    preflightCommitment: "recent",
-  });
-
-  const idl = await Program.fetchIdl(NAMESPACE_ID, provider);
-
-  const program = new Program(idl, NAMESPACE_ID, provider);
-
-  return new NamespaceProgram({
-    id: NAMESPACE_ID,
-    program,
-  });
-}
+    const namespaceObj = await this.client.account.namespace.fetch(namespacePDA);
+    return new Namespace(namespacePDA, namespaceObj);
+  };
+};
---
+++ js/src/instructions/namespace.ts
@@ -0,0 +1,65 @@
+import { web3, BN, AnchorProvider } from "@project-serum/anchor";
+import { SystemProgram } from "@solana/web3.js";
+import {
+  Program,
+  Instruction as SolKitInstruction,
+  InstructionUtils
+} from "@raindrops-protocol/sol-kit";
+
+import { getNamespacePDA } from "../utils/pda";
+import { TOKEN_PROGRAM_ID } from "../constants/programIds";
+import { PermissivenessSettings } from "../state/namespace";
+
+export interface InitializeNamespaceAccounts {
+  namespace?: web3.PublicKey;
+  mint: web3.PublicKey;
+  metadata: web3.PublicKey;
+  masterEdition: web3.PublicKey;
+}
+
+export interface InitializeNamespaceArgs {
+  desiredNamespaceArraySize: BN;
+  uuid: string;
+  prettyName: string;
+  permissivenessSettings: PermissivenessSettings;
+  whitelistedStakingMints: web3.PublicKey[];
+}
+
+export class Instruction extends SolKitInstruction {
+  constructor(args: { program: Program.Program }) {
+    super(args);
+  }
+
+  async initializeNamespace(
+    args: InitializeNamespaceArgs,
+    accounts: InitializeNamespaceAccounts,
+  ) {
+    const [namespacePDA, _namespaceBump] = await getNamespacePDA(accounts.mint);
+
+    InstructionUtils.convertNumbersToBNs(args, [
+      "desiredNamespaceArraySize",
+      "bump",
+    ])
+
+    const remainingAccounts = args.whitelistedStakingMints.map((mint) => {
+      return { pubkey: mint, isWritable: false, isSigner: false };
+    });
+
+    return [
+      await this.program.client.methods
+        .initializeNamespace(args)
+        .accounts({
+          namespace: namespacePDA,
+          mint: accounts.mint,
+          metadata: accounts.metadata,
+          masterEdition: accounts.masterEdition,
+          payer: (this.program.client.provider as AnchorProvider).wallet.publicKey,
+          tokenProgram: TOKEN_PROGRAM_ID,
+          systemProgram: SystemProgram.programId,
+          rent: web3.SYSVAR_RENT_PUBKEY,
+        })
+        .remainingAccounts(remainingAccounts)
+        .instruction(),
+    ];
+  }
+}
