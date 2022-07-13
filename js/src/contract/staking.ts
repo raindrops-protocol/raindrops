7b7523d880ff5d326965a61f9b04fb560f2bfcec
cotemper <cotemper@gmail.com>
2022-07-14 00:28:30 +0800

    add staking contracts

---
+++ js/src/contract/staking.ts
@@ -0,0 +1,73 @@
+import { web3 } from "@project-serum/anchor";
+import { Program } from "@raindrops-protocol/sol-kit";
+import { STAKING_ID } from "../constants/programIds";
+import { PREFIX } from "../constants/staking";
+import * as StakingInstruction from "../instructions/staking";
+
+export class StakingProgram extends Program.Program {
+  declare instruction: StakingInstruction.Instruction;
+  static PREFIX = PREFIX;
+  PROGRAM_ID = STAKING_ID;
+
+  constructor() {
+    super();
+
+    this.instruction = new StakingInstruction.Instruction({ program: this });
+  }
+
+  async beginArtifactStakeWarmup(
+    args: StakingInstruction.BeginArtifactStakeWarmupArgs,
+    accounts: StakingInstruction.BeginArtifactStakeWarmupAccounts,
+    options?: { commitment: web3.Commitment; timeout?: number }
+  ) {
+    const instruction = await this.instruction.beginArtifactStakeWarmup(
+      args,
+      accounts
+    );
+
+    return await this.sendWithRetry(
+      instruction,
+      [accounts.stakingTransferAuthority],
+      options
+    );
+  }
+
+  async endArtifactStakeWarmup(
+    args: StakingInstruction.EndArtifactStakeWarmupArgs,
+    accounts: StakingInstruction.EndArtifactStakeWarmupAccounts,
+    options?: { commitment: web3.Commitment; timeout?: number }
+  ) {
+    const instruction = await this.instruction.endArtifactStakeWarmup(
+      args,
+      accounts
+    );
+
+    return await this.sendWithRetry(instruction, [], options);
+  }
+
+  async beginArtifactStakeCooldown(
+    args: StakingInstruction.BeginArtifactStakeCooldownArgs,
+    accounts: StakingInstruction.BeginArtifactStakeCooldownAccounts,
+    options?: { commitment: web3.Commitment; timeout?: number }
+  ) {
+    const instruction = await this.instruction.beginArtifactStakeCooldown(
+      args,
+      accounts
+    );
+
+    return await this.sendWithRetry(instruction, [], options);
+  }
+
+  async endARtifactStakeCooldown(
+    args: StakingInstruction.EndArtifactStakeCooldownArgs,
+    accounts: StakingInstruction.EndArtifactStakeCooldownAccounts,
+    options?: { commitment: web3.Commitment; timeout?: number }
+  ) {
+    const instruction = await this.instruction.endArtifactStakeCooldown(
+      args,
+      accounts
+    );
+
+    return await this.sendWithRetry(instruction, [], options);
+  }
+}
